4b849197d2f0fb380c4e9c12174248cb8ebc688c
Aidan Sweeny <apswee22@colby.edu>
2020-08-11 20:18:55 -0400

    Fixing heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static("public"))
 require('./routes/api_routes')(app)
 require('./routes/html_routes')(app)
 
-db.sequelize.sync({ force: false }).then(function () {
+db.sequelize.sync({ force: true }).then(function () {
     app.listen(PORT, function () {
         console.log("Server listening on localhost:" + PORT)
     })
